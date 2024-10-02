4a08d854c571d99c950e5e469208c9d56404f5d1
Stefan Marinovic <stefan.marinovic06@hotmail.com>
2024-10-02 15:09:10 +0200

    Aufgaben zu Methoden und Schleifen.

---
+++ StefanMarinovic/Week01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace StefanMarinovic
+{
+
+    class Programm 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me a height and I will make a square for you");
+            string response = Console.ReadLine();
+            if (int.TryParse(response, out int number))
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i == 1)
+                {
+                    for (int j = 1; j <= number; j++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.WriteLine();
+                }
+                else if (i == number)
+                {
+                    for (int j = 1; j <= number; j++)
+                    {
+                        Console.Write("X");
+                    }
+                }
+                else
+                {
+                    Console.Write("X");
+                    for (int j = 3; j <= number; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("X");
+                    Console.WriteLine();
+                } 
+            }
+        }
+    }
+}
