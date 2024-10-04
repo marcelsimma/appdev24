7dd85a21104f581873eb8a1afe49ccd2ab01f5c3
Manuel Hartmann <HARMANU@HILTI.com>
2024-10-04 09:21:46 +0200

    Edit

--- ManuelHartmann/Week01/CalcSum.cs
+++ ManuelHartmann/Week01/CalcSum.cs
@@ -12,14 +12,16 @@ namespace ManuelHartmann
         static void CalcSum(string[] args)
         {
             int sum = 20;
-            for (int i = 20; i <= 50; i++)
+            Console.WriteLine("Sum of 20 to 50 added together");
+            Console.WriteLine(new string('-', 25));
+            for (int i = 21; i <= 50; i++)
             {
-                Console.Write($"Rechenweg: {sum} + {i} = ");
+                Console.Write($"Calculation path: {sum} + {i} = ");
                 sum = Add(sum, i);
                 Console.WriteLine(sum);
             }
-            Console.WriteLine();
-            Console.WriteLine($"Gesamtwert = {sum}");
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine($"Total value = {sum}");
         }
         public static int Add(int x, int y)
         {
