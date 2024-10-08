46280c2afac659ec3fc00be52533ed90b4383fa7
Karuma525 <stroehle.julian@gmail.com>
2024-10-08 13:58:06 +0200

    new code

--- JulianStroehle/JulianStroehle/Program.cs
+++ JulianStroehle/JulianStroehle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using JulianStroehle;
 using JulianStroehle.Week1;
 using JulianStroehle.Week2;
@@ -8,7 +9,27 @@ namespace JulianStroehle
     {
         static void Main(string[] args)
         {
-            FactorialA.Factorial(20);
+            // Woche 1
+            // GrossesEinMalEinsA.GrossesEinMalEins();
+            // MalreiheA.Malreihe();
+            // PrintA.Print();
+            // PrintDeltoidA.PrintDeltoid(9, '*');
+            // PrintEmptySquareA.PrintEmptySquare();
+            // SummeA.calcSum(5, 10);
+            // SummeB.Summen(5, 10);
+            // Tree.RandomTree();
+            // TannenbaumA.Tannenbaum();
+            // TannenbaumB.Tannenbaum2();
+            // DurchZweiB.DurchZwei();
+
+            // Woche 2
+            // CounterA.Counter1();
+            // CounterA.Counter2();
+            // TeilenA.Teilen();
+            // FactorialA.Factorial(20);
+            // CalculatePI.PI();
+            LicenseA.License1();
+            // DezHexA.DezHex();
         }
     }
 }
