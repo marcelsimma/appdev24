b8dd8a75c17059d74c0aae854ff4c4c8fbb38965
AustrianStrohii <simon.jochum@lbsfe1.snv.at>
2024-10-08 14:32:20 +0200

    add DrivingLicense.cs

---
+++ SimonJochum/SimonJochum/Week02/DrivingLicense.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace SimonJochum.Week02
+{
+    public class DrivingLicense
+    {
+        private static string name;
+        private static int age;
+        private static bool check1;
+        private static bool check2;
+        public static void Start()
+        {
+            Console.WriteLine("Hallo, bitte gib deinen Namen ein.");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hallo {name}, wie alt bist du?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            CheckMoped();
+            CheckCar();
+
+            if (check1 == true && check2 == true)
+            {
+                Console.WriteLine($"{name} du darfst Moped und Auto fahren");
+            }
+            else if (check1 == true && check2 == false)
+            {
+                Console.WriteLine($"{name} du darfst nur Moped fahren.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} du darfst weder Moped noch Auto fahren.");
+            }
+        }
+
+        public static void CheckMoped()
+        {
+            
+            if (age >= 15)
+            {
+                check1 = true;
+            }
+            else
+            {
+                check1 = false;
+            }
+
+        }
+        public static void CheckCar()
+        {
+            if (age >= 18)
+            {
+                check2 = true;
+            }
+                
+            else
+            {
+                check2 = false;
+            }
+        }
+    }
+}
