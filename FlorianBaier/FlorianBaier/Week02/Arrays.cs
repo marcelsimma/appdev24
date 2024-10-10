85505e8b00d4ce6eee05726e41878cd0c6d94738
FlorianBaier17 <florian.baier17@gmail.com>
2024-10-10 16:01:36 +0200

    Changes

--- FlorianBaier/FlorianBaier/Week02/Arrays.cs
+++ FlorianBaier/FlorianBaier/Week02/Arrays.cs
@@ -19,9 +19,29 @@ namespace FlorianBaier.Week02
                 randomNumbers[i] = random.Next(-50, 51);
                 Console.WriteLine(randomNumbers[i]);
             }
+            
+            int sum = 0;
+            
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                if (randomNumbers[i] > 0)
+                {
+                    sum += randomNumbers[i];
+                }
+                else
+                {
+                    sum += randomNumbers[i] * (-1);
+                }
+            }
+            
+            double averageAbsValue = sum / randomNumbers.Length;
+
             Console.WriteLine($"Die größte Zahl des Array ist {randomNumbers.Max()}");
             Console.WriteLine($"Die kleinste Zahl des Array ist {randomNumbers.Min()}");
             Console.WriteLine($"Der Mittelwert ist {randomNumbers.Average()}");
+            Console.WriteLine($"Der Betrag ist {averageAbsValue}");
+
+
         }
     }
 }
