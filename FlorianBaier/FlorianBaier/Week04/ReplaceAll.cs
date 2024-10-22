b2a58ae2b9b3946750d2fc95f0a3765af3141857
FlorianBaier17 <florian.baier17@gmail.com>
2024-10-22 15:46:15 +0200

    Update

--- FlorianBaier/FlorianBaier/Week04/ReplaceAll.cs
+++ FlorianBaier/FlorianBaier/Week04/ReplaceAll.cs
@@ -13,47 +13,11 @@ namespace FlorianBaier.Week04
             Console.WriteLine(replacedText);
         }
 
-        public static void DeleteLowercase()
+        
+
+         public static string GetText()
         {
-            string stringExample = GetText();
-            string output = new string(stringExample.Where(Char.IsUpper).ToArray());
-            Console.WriteLine("1." + output);
-        }
- 
-        public static void DeleteUppercase()
-        {
-            string stringExample = GetText();
-            string output = new string(stringExample.Where(Char.IsLower).ToArray());
-            Console.WriteLine("2." + output);
-        }
- 
-        public static void DeleteWhitespaces()
-        {
-            string stringExample = GetText();
-            string output = new string(stringExample.Where(c => !Char.IsWhiteSpace(c)).ToArray());
-            Console.WriteLine("3." + output);
-        }
- 
-        public static void DeleteExclamationMark()
-        {
-            string stringExample = GetText();
-            string toDelete = "!";
-            string forDelete = "";
-            int pos = 0;
-            int count = 0;
-            string? replacedText;
- 
-            pos = stringExample.IndexOf(toDelete, 0);
-            replacedText = stringExample.Replace(toDelete, forDelete);
- 
-            while (pos >= 0)
-            {
-                count++;
-                pos = stringExample.IndexOf(toDelete, pos + 1);
-                replacedText = stringExample.Replace(toDelete, forDelete);
-            }
-            Console.WriteLine("4." + replacedText);
- 
+            return "Hallo, ich bin 26 Jahre alt, lebe in Wolfurt und lerne das Programmieren von C# im Digital Campus.";
         }
 
         }
