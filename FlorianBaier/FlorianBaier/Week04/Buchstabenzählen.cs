3e12a5fa10acdedfe4a19606c18bf04abcd6da8c
FlorianBaier17 <florian.baier17@gmail.com>
2024-10-22 10:59:52 +0200

    week04 new

--- FlorianBaier/FlorianBaier/Week04/Buchstabenzählen.cs
+++ FlorianBaier/FlorianBaier/Week04/Buchstabenzählen.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace FlorianBaier.Week03
+namespace FlorianBaier.Week04
 {
 
     public class Buchstabenzaehlen
@@ -8,8 +8,62 @@ namespace FlorianBaier.Week03
 
         public static void Start()
         {
-            Console.WriteLine("Gib eine Zeichenfolge oder einen Text ein: ");
-            string? text = Console.ReadLine();
+            {
+                string userInput = GetText();
+
+                int totalLetters = 0;
+                int totalDigits = 0;
+                int totalWhitespaces = 0;
+                int totalSpecialChars = 0;
+                Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+
+                char[] characters = userInput.ToCharArray();
+
+                foreach (char c in characters)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        totalLetters++;
+                        char lowerC = char.ToLower(c);
+                        if (letterCounts.ContainsKey(lowerC))
+                        {
+                            letterCounts[lowerC]++;
+                        }
+                        else
+                        {
+                            letterCounts[lowerC] = 1;
+                        }
+                    }
+                    else if (char.IsDigit(c))
+                    {
+                        totalDigits++;
+                    }
+                    else if (char.IsWhiteSpace(c))
+                    {
+                        totalWhitespaces++;
+                    }
+                    else
+                    {
+                        totalSpecialChars++;
+                    }
+                }
+
+                Console.WriteLine($"Number of letters: {totalLetters}");
+                Console.WriteLine($"Number count: {totalDigits}");
+                Console.WriteLine($"Whitespaces: {totalWhitespaces}");
+                Console.WriteLine($"Special characters: {totalSpecialChars}");
+
+                Console.WriteLine("\nLetter frequency:");
+                foreach (var pair in letterCounts)
+                {
+                    Console.WriteLine($"'{pair.Key}': {pair.Value}");
+                }
+            }
+        }
+        public static string GetText()
+        {
+            return "Hallo, ich bin 26 Jahre alt, lebe in Wolfurt und lerne das Programmieren von C# im Digital Campus.";
         }
     }
 }
+
