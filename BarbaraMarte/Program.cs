b6edd5033f7d537d3178857edd53053a59f47930
Barbara Marte <bmarte343@gmail.com>
2024-11-05 09:48:17 +0100

    Update1

--- BarbaraMarte/Program.cs
+++ BarbaraMarte/Program.cs
@@ -3,6 +3,8 @@ using BarbaraMarte.Week01;
 using BarbaraMarte.Week02;
 using BarbaraMarte.Week03;
 using BarbaraMarte.Week04;
+using BarbaraMarte.Schule;
+using BarbaraMarte.Work;
 
 
 namespace BarbaraMarte.Week01
@@ -11,7 +13,20 @@ namespace BarbaraMarte.Week01
     {
         static void Main()
         {
+            //BinaryRechner.Print();
+            Kontaktverwaltung.Print();
+            //CalenderBlueprint.Zeitpunkt();
 
+            //WordFinder.Letters();
+            //RegexMail.Print();
+            // MyBirthday.Print();
+            //WannUndWo.Print();
+            // WhenIsSunday.Print();
+            //HowManyWorkdays.Print();
+            //Kallender.Print();
+            //CashChange.InputAndConvertToCent();
+            // MoneyCounter.Print(400);
+            // TicTacToe.Print();
         }
     }
 }
---
+++ BarbaraMarte/Week04/Kallender.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BarbaraMarte.Week04;
+
+class Kallender
+{
+    public static void Print()
+    {
+        Console.WriteLine("{0, -15} {1, -10} {2, -11} {3, -13} {4, -12} {5, -9} {6, -12} {7, -10}", "Month", "Monday", "Tuesday", "Wednesday", "Thursday", "Fiday", "Saturday", "Sunday");
+        DateTime dateTime = new DateTime();
+        int count = 0;
+        //int Monday[] ={ };
+
+
+      //   Console.WriteLine("{0, -15} {1, -10} {2, -11} {3, -13} {4, -12} {5, -9} {6, -12} {7, -10}", "     ", Monday[i], Tuesday[i], Wednesday[i], Thursday[i], Fiday[i], Saturday[i], Sunday[i]);
+    }
+}
