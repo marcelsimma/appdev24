67804a399fa22822be97c6f31d52c4ebd3d2ed66
CheyenneHarbarth <cheyenne.harbarth@blum.com>
2024-11-07 10:35:45 +0100

    Updates Woche 5

--- CheyenneHarbarth/Week05/Streams/Stream.cs
+++ CheyenneHarbarth/Week05/Streams/Stream.cs
@@ -37,17 +37,56 @@ namespace CheyenneHarbarth.Week05.Streams
                     string[] Names = new string[12];
                     Names[i] = str.ReadLine();
 
-                    /*  using (StreamWriter stro = new StreamWriter("/Users/BAU28365/Documents/output.txt"))
-                     {
-                         stro.WriteLine($"{counter,3}. {Names[i]}");
-                         stro.WriteLine($"{counter} Teilnehmer");
-                     } */
+                    using (StreamWriter stro = new StreamWriter("/Users/BAU28365/Documents/output.txt"))
+                    {
+                        stro.WriteLine($"{counter,3}. {Names[i]}");
+                        stro.WriteLine($"{counter} Teilnehmer");
+                    }
                     Console.WriteLine($"{counter,3}. {Names[i]}");
                     counter++;
                 }
-
             }
             Console.WriteLine("fertig");
+        }
+
+        public static void Aufgabe3()           //Quadrieren
+        {
+            using (StreamReader str = new StreamReader("/Users/BAU28365/Documents/numbers.txt"))
+            {
+                int a = Convert.ToInt32(str.ReadLine());
+                int b = Convert.ToInt32(str.ReadLine());
+                int c = Convert.ToInt32(str.ReadLine());
+                int d = Convert.ToInt32(str.ReadLine());
+                int e = Convert.ToInt32(str.ReadLine());
+
+
+
+                using (StreamWriter stro = new StreamWriter("/Users/BAU28365/Documents/results.txt"))
+                {
+                    int f = a * a;
+                    int g = b * b;
+                    int h = c * c;
+                    int i = d * d;
+                    int j = e * e;
+
+                    stro.WriteLine(f + " = " + a + " * " + a);
+                    stro.WriteLine($"{g} = {b} * {b}");
+                    stro.WriteLine($"{h} = {c} * {c}");
+                    stro.WriteLine(i + " = " + d + " * " + d);
+                    stro.WriteLine(j + " = " + e + " * " + e);
+                }
+            }
+            Console.WriteLine("fertig");
+        }
+        public static void Aufgabe4()
+        {
+            int i = 2;
+            using (StreamReader str = new StreamReader("/Users/BAU28365/Documents/Liste.txt"))
+            {
+                Console.WriteLine("Teilnehmer  |  Größe   | Alter  ");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine(str.Read());
+            }
         }
     }
 }
