ad100647b5625e9a8fb8c9ff7694deb1a33701d1
FlorianBaier17 <florian.baier17@gmail.com>
2024-11-19 13:11:48 +0100

    added Ticketsales

---
+++ FlorianBaier/FlorianBaier/Week06/Ticketsales/Ticketsales.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace FlorianBaier.Week06
+{
+    public class Ticketsales
+    {
+        Ticketprovider Eventim = new Ticketprovider();
+
+        Concert lilPeep = new Concert("Rock Concert");
+        Concert juiceWrld = new Concert("Jazz Concert");
+
+        Person person1 = new Person("Florian");
+        Person person2 = new Person("Melanie");
+        Person person3 = new Person("Günther");
+        Person person4 = new Person("Margit");
+    }
+}
+
