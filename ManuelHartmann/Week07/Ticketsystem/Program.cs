f6a8144dd39d5f699db1a6d4e1798e07b3e908ce
Manuel Hartmann <HARMANU@HILTI.com>
2024-11-19 16:32:55 +0100

    Ticketsystem

---
+++ ManuelHartmann/Week07/Ticketsystem/Program.cs
@@ -0,0 +1,53 @@
+namespace Ticketsystem
+{
+    public class Program
+    {
+        /*
+
+        1. Grundannahmen definieren und Context abstecken
+        Grundannahme: Highend Kamera wo Objektiv und Speicherkarte tauschbar sind. 
+
+        2. Klassendiagramm erstellen mit allen Klassen(, Interfaces, Beziehungen, etc.)
+
+        3. Klassen programmieren
+        */
+
+        public static void Launch()
+        {
+            // Erstelle Ticket-Anbieter und Konzerte
+            TicketAnbieter ticketAnbieter = new TicketAnbieter();
+
+            Konzert konzert1 = new Konzert("Rock am Ring", 5);
+            Konzert konzert2 = new Konzert("Jazz Night", 50);
+
+            // Personen einreihen ( Programmablauf 1)
+            Bestellungen bestellung1 = new Bestellungen();
+            Bestellungen bestellung2 = new Bestellungen();
+
+            bestellung1.PersonEinreihung(new Personen("Stefan", 2, konzert1.Name));
+            bestellung1.PersonEinreihung(new Personen("Hannes", 5, konzert1.Name));
+            bestellung1.PersonEinreihung(new Personen("Andreas", 1, konzert1.Name));
+            bestellung2.PersonEinreihung(new Personen("Bob", 5, konzert2.Name));
+            bestellung2.PersonEinreihung(new Personen("Charlie", 8, konzert2.Name));
+            bestellung2.PersonEinreihung(new Personen("Diana", 2, konzert2.Name));
+            bestellung2.PersonEinreihung(new Personen("Lucy", 10, konzert2.Name));
+
+            for (int i = 0; i < bestellung1.Bestellungsdetails.Count; i++)
+            {
+                konzert1.TicketVerkaufen(bestellung1);
+            }
+            
+            konzert2.TicketVerkaufen(bestellung2);
+
+            // Simuliere den Ticketkauf (Programmablauf 2)
+
+            Console.WriteLine();
+
+            // Ticketkauf fuer 'Rock am Ring'
+            ticketAnbieter.GekaufteTickets.Add(konzert1, bestellung1);
+            ticketAnbieter.GekaufteTickets.Add(konzert2, bestellung2);
+
+            ticketAnbieter.AusgabeGekaufteTickets();
+        }
+    }
+}
---
+++ ManuelHartmann/Week07/Ticketsystem/TicketAnbierter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketsystem
+{
+    public class TicketAnbieter
+    {
+        public Dictionary<Konzert, Bestellungen> GekaufteTickets { get; private set; }
+
+        public TicketAnbieter()
+        {
+            GekaufteTickets = new Dictionary<Konzert, Bestellungen>();
+        }
+        public void AusgabeGekaufteTickets()
+        {
+            Console.WriteLine("\nListe der gekauften Tickets: ");
+            Console.WriteLine(new string('-', 30));
+
+            foreach (var eintrag in GekaufteTickets)
+            {
+                foreach(Personen p in eintrag.Value.Bestellungsdetails)
+                {
+                    Console.WriteLine($"{p.PersonenName} hat sich erfolgreich n: {p.Ticktanzahl} fuer das Konzert '{p.KonzertName}' gekauft.");
+                }
+            }
+        }
+    }
+}
