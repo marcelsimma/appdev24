6e2821f6f0738ac7f6358d94f3f9a4258e3f2f74
FatmanurKeles <ftmnurkls16@gmail.com>
2024-11-27 14:49:50 +0100

    update

--- FatmanurKeles/Week06/AufgabeZoo/ZooStart.cs
+++ FatmanurKeles/Week06/AufgabeZoo/ZooStart.cs
@@ -1,3 +1,4 @@
+using FatmanurKeles.Week06.AufgabeZoo;
 using FatmanurKeles.Week06.ZOO;
 
 namespace FatmanurKeles.Week06.ZOO
@@ -6,12 +7,22 @@ namespace FatmanurKeles.Week06.ZOO
     {
         public static void Launch()
         {
+            List<Gehege> alleGehegen = new List<Gehege>();
             Gehege alpenwiese = new Gehege("Alpenwiese Montafon");
             Gehege ried = new Gehege("Ried");
             Gehege terrarium = new Gehege("Terrarium (warm)");
 
-            Console.WriteLine(alpenwiese.ToString());
+            alleGehegen.Add(alpenwiese);
+            alleGehegen.Add(ried);
+            alleGehegen.Add(terrarium);
+            Zoo beschreibung = new Zoo("Tiergarten Feldkirch", "2018", ried, "Gisingen");
+            
+            
+            foreach (Gehege gehegen in alleGehegen)
+            {
+                Console.WriteLine(gehegen.ToString());
+            }
+            Console.WriteLine(new string('-', 32));
         }
     }
 }
-
