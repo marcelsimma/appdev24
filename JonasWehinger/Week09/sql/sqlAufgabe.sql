dac14459129c8c901dc6de48799d220510e58f8e
jonas.wehinger <jonas.wehinger@lbsfe1.snv.at>
2024-12-03 13:45:56 +0100

    SQL

--- JonasWehinger/Week09/sql/sqlAufgabe.sql
+++ JonasWehinger/Week09/sql/sqlAufgabe.sql
@@ -67,6 +67,17 @@ AND cy.code = rele.country
 AND rele.Name LIKE 'MUSLIM'
 AND sea LIKE 'Pacific Ocean'
 AND rele.percentage > 60;
+
+USE Mondial;
+SELECT percentage, rele.name, sea
+FROM  Island iz, geo_island glan, Country cy, religion rele, islandin izi
+WHERE izi.island = iz.name
+AND iz.name = glan.island
+AND glan.country = cy.Code
+AND cy.code = rele.country
+AND rele.Name LIKE 'MUSLIM'
+AND sea LIKE 'Pacific Ocean'
+AND rele.percentage > 50;
 -- Aufgabe 10
 USE Mondial;
 SELECT gmoun.Country, Height, rele.name
@@ -78,4 +89,126 @@ AND rele.Name LIKE 'Roman Catholic'
 AND Height >3000
 AND rele.Percentage > 60
 --Aufgabe 11
+/*USE Mondial;
+SELECT SUM(Population) AS Population, Rele.name 
+FROM religion rele, country cy, encompasses ec, Continent ct
+WHERE ct.name = ec.continent
+AND ec.country  = cy.code
+AND cy.code =  rele.country
+Group by rele.name
+*/--Aufgabe 12
+USE mondial;
+SELECT ct.name , ty.country,ty.name,ty.population
+From continent ct , encompasses en, Country cy, City ty
+WHERE ty.country = cy.code
+AND ct.name = en.Continent
+AND en.Country = cy.code
+AND  ct.name LIKE 'America'
+ORDER BY population DESC
+LIMIT 3
+-- Aufgabe 13
+USE mondial;
+SELECT en.Continent ,SUM(cy.population) / (SELECT SUM(cy.population)
+FROM country cy) * 100 as Summe
+FROM country cy, encompasses en
+WHERE cy.code = en.country
+AND en.Continent  like 'Europe'
+GROUP BY en.continent;
+-- Aufgabe 14
 
+-- Aufgabe 15
+SELECT Count(cy.name) AS citycount, ct.name
+FROM city cy, country ct
+WHERE cy.country = ct.code
+GROUP BY ct.name
+HAVING citycount = 3;
+-- Aufgabe 16
+SELECT en.Continent, MAX(mou.Height) AS Height
+FROM mountain mou, encompasses en, geo_mountain gmoun
+WHERE mou.Name = gmoun.Mountain
+AND gmoun.Country = en.Country
+GROUP BY en.Continent;
+-- Aufgabe 17
+SELECT org.Name, COUNT(meme.Organization) AS Members 
+FROM organization org, ismember meme
+WHERE meme.Organization = org.Abbreviation AND org.Country = 'A'
+GROUP BY org.Name
+ORDER BY org.Name;
+-- Aufgabe 18
+select gmoun.Country,lk.name AS Seaname ,gmoun.Mountain
+from mountain mt, geo_mountain gmoun, lake lk , geo_lake glk
+WHERE glk.Lake = lk.name
+AND lk.Depth > 100
+AND mt.Height > 1500
+AND gmoun.Mountain = mt.Name 
+ORDER BY gmoun.country;
+-- Aufgabe 19
+SELECT  cy.Name , gsea.Sea
+FROM Country cy, geo_sea gsea
+WHERE cy.Code = gsea.Country;
+-- Aufgabe 20
+insert INTO organization value('DCVG', 'DCV-Grundkurs','ofcompton','A','Voralberg','2001.12.03' );
+ 
+ 
+insert INTO ismember Values ('A','DCVG','member');
+insert INTO ismember Values ('BRU','DCVG','member');
+insert INTO ismember Values ('BS','DCVG','member');
+-- Aufgabe 21
+SELECT * FROM mondial.ismember WHERE Organization = 'DCVG'
+-- Aufgae 22
+UPDATE ismember
+SET ismember.Organization = 'ADCVG' WHERE country = 'A'
+and ismember.Organization = 'DCVG';
+
+UPDATE ismember
+SET ismember.Organization = 'ADCVG' WHERE country = 'BRU'
+and ismember.Organization = 'DCVG';
+
+UPDATE ismember
+SET ismember.Organization = 'ADCVG' WHERE country = 'BS'
+and ismember.Organization = 'DCVG' ;
+-- Aufgabe 23
+DELETE FROM organization WHERE Abbreviation = 'DCVG'
+-- Aufgabe 24
+UPDATE mountain
+SET Mountain.Height = '3798' WHERE name = 'Großglockner'
+-- Aufgabe 25
+Insert Into Country(Name,Code,Capital,Province,Area,Population)
+Value ('Transnistrien','Tra','capitalCity','capitalPro','20541','23')
+Insert Into encompasses(Country,Continent,Percentage)
+Value('Tra','Europe','100')
+-- Aufgabe 26
+INSERT INTO geo_mountain
+VALUES ('MouBig', 'Tra', 'capitalPro');
+INSERT INTO geo_mountain
+VALUES ('BigMou', 'Tra', 'capitalPro');
+INSERT INTO mountain
+VALUES ('MouBig', NULL, 580, NULL, NULL, NULL);
+INSERT INTO mountain
+VALUES ('BigMou', NULL, 270, NULL, NULL, NULL);
+INSERT INTO Province
+VALUES ('CapitalPro','Tra','2','55','capitalCity','capitalPro')
+INSERT INTO city
+VALUES ('capitalCity', 'Tra', 'capitalPro', 12432, NULL, NULL);
+-- Aufgabe 27
+UPDATE country SET Name = 'Türkiye' WHERE Name = 'Turkey';
+-- Aufgabe 28
+UPDATE province, country
+SET province.Population = country.Population / 16
+WHERE province.Country = country.Code
+AND province.Country = 'D';
+--Aufgabe 29
+Update Country SET Capital = 'Nusantara' WHERE Capital = 'Bangkok'
+
+Update City SET name = 'Nusantara' WHERE name = 'Bangkok'
+AND Province = 'Thailand'
+AND country = 'THA'
+--Aufgabe 30
+INSERT INTO organization VALUES ('VG', 'Visegard Group', 'Visegard', 'H', 'Brudapest ', '2001-02-11')
+ 
+INSERT INTO ismember VALUES ('H', 'VG', 'member')
+INSERT INTO ismember VALUES ('A', 'VG', 'member')
+INSERT INTO ismember VALUES ('CZ', 'VG', 'member')
+INSERT INTO ismember VALUES ('CH', 'VG', 'member')
+INSERT INTO ismember VALUES ('VG', 'VG', 'Chef')
+--Aufgabe 31
