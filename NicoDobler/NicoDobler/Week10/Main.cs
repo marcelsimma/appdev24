91007307d2d63f5fd3e786ca089fbbb28d212de8
nicodobler23 <dobler@hgisystems.com>
2024-12-12 15:39:39 +0100

    update

---
+++ NicoDobler/NicoDobler/Week10/Main.cs
@@ -0,0 +1,22 @@
+using System;
+ 
+ 
+namespace NicoDobler.Week10;
+ 
+class Zoo1
+{
+    public static void los()
+    {
+        Zoo zoo = new Zoo("Tiergarten Dornbirn", 2022);
+ 
+        Gehege gehege1 = new Gehege("Alpenwiese");
+        Gehege gehege2 = new Gehege("Aquarium");
+        Gehege gehege3 = new Gehege("Terrarium");
+ 
+        zoo.GehegeHinzufuegen(gehege1);
+        zoo.GehegeHinzufuegen(gehege2);
+        zoo.GehegeHinzufuegen(gehege3);
+ 
+        zoo.StrukturAusgeben();
+    }
+}
